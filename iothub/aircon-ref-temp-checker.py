9c5d2f2dcff65ef4c10d6ba421cb42d7e30e94e8
Daisuke Kodaira <43132698+daisukekodaira@users.noreply.github.com>
2024-04-05 16:09:03 +0900

    Add files via upload

---
+++ iothub/aircon-ref-temp-checker.py
@@ -0,0 +1,132 @@
+# エアコンの設定温度を変更して状態取得（SET＆GETの繰り返し）し、設定可能なエアコン温度を確認する
+# - 引数
+#   - SET内容（EPC、値）
+#   - GET間隔（秒数）
+# - 動作
+#   1. コード中で指定されたSET内容の事前GETを実施
+#   2. コード中で指定されたSET内容をもとにSET信号を送信
+#   3. コード中で指定された間隔だけWAITしたあとに、エアコンの設定温度を変更し、手順1. 手順 2. を繰り返す
+# - 出力
+#   - GET, SETの状態出力
+
+import requests
+import json
+import os
+from dotenv import load_dotenv
+import time, datetime
+import threading
+
+load_dotenv()
+
+url = os.environ['TARGET_URL']
+i = 0
+
+payload0 = {
+  "requests": [
+    {
+      "command": [
+        {
+        "command_type": "character",
+        "command_code": "get_property_value",
+        "command_value": "targetTemperature"
+        }
+      ],
+      "driver_id": os.environ['DRIVER_ID'],
+      "r_edge_id": os.environ['R_EDGE_ID'],
+      "thing_uuid": os.environ['THING_UUID_2F_S']
+    }
+  ]
+}
+
+payload1 = {
+  "requests": [
+    {
+      "command": [
+        {
+        "command_type": "character",
+        "command_code": "set_property_value",
+        "command_value": "targetTemperature=25"
+        }
+      ],
+      "driver_id": os.environ['DRIVER_ID'],
+      "r_edge_id": os.environ['R_EDGE_ID'],
+      "thing_uuid": os.environ['THING_UUID_2F_S']
+    }
+  ]
+}
+
+targetTemperatures = {
+    "0": 17,
+    "1": 17.5,
+    "2": 18,
+    "3": 18.5,
+    "4": 19,
+    "5": 20,
+    "6": 21,
+    "7": 22,
+    "8": 23,
+    "9": 24,
+    "10": 25,
+    "11": 26,
+    "12": 27,
+    "13": 28,
+    "14": 29,
+    "15": 30,
+    "16": 31,
+    "17": 31.5,
+    "18": 32,
+    "19": 32.5,
+    "20": 33
+}
+
+headers = {
+  "Content-type": "application/json",
+  "Authorization": "Bearer "+os.environ['ACCESS_TOKEN'],
+  "X-IOT-API-KEY": os.environ['API_KEY']
+}
+
+def getRequest(pl):
+    response = requests.request("POST", url, headers=headers, json=pl)
+
+    # print(response.text)
+    jsonData = response.json()
+
+    jsonDict = {
+    'command_code': jsonData['results'][0]["command"][0]["command_code"],
+    'command_value': jsonData['results'][0]["command"][0]["command_value"],
+    'response_result': jsonData['results'][0]["command"][0]["response"][0]["response_result"],
+    'response_value': jsonData['results'][0]["command"][0]["response"][0]["response_value"]
+    }
+
+    print(jsonDict["command_code"]  + " (" + jsonDict["command_value"] + ") " + 
+    jsonDict["response_result"] + " (" + jsonDict["response_value"] + ")")
+
+
+def getRequests():
+    global i
+    print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y/%m/%d %H:%M:%S'))
+    # global payload1
+    
+    # print(payload1['requests'][0]['command'][0]['command_value'])
+    payload1['requests'][0]['command'][0]['command_value']= "targetTemperature=" + str(targetTemperatures[str(i)])
+    print(i)
+    i+=1
+
+    getRequest(payload0)
+
+    time.sleep(5)
+
+    getRequest(payload1)
+
+def scheduler(interval, f, wait = True):
+    base_time = time.time()
+    next_time = 0
+    while True:
+        t = threading.Thread(target = f)
+        t.start()
+        if wait:
+            t.join()
+        next_time = ((base_time - time.time()) % interval) or interval
+        time.sleep(next_time)
+
+scheduler(10, getRequests, True)
---
+++ iothub/set_and_get.py
@@ -0,0 +1,131 @@
+# 遠方操作でDER機器をSETした項目の状態確認（SET＆GET）
+# - 引数
+#   - なし
+# - 動作
+#   1. コード文中のSET内容の事前GETを実施
+#   2. コード文中のSET内容をもとにSET信号を送信
+#   3. コード文中で設定された秒数だけWAITしたあとに、GET信号を送信
+# - 出力
+#   - GET, SET の結果をそれぞれコンソールへ表示する
+
+import requests
+import json
+import os
+from dotenv import load_dotenv
+import time
+# import sys
+
+load_dotenv()
+
+url = os.environ['TARGET_URL']
+
+payload_get = {
+  "requests": [
+    {
+      "command": [
+        {
+        "command_type": "character",
+        "command_code": "get_property_value",
+        "command_value": "instantaneousChargingAndDischargingElectricPower"
+        }
+      ],
+      #######
+      # 遠方操作対象のDER機器を指定してください（DRIVER_ID, R_EDGE_ID, THINGS_UUID）
+      #######
+      "driver_id": os.environ['DRIVER_ID'], 
+      "r_edge_id": os.environ['R_EDGE_ID'],
+      "thing_uuid": os.environ['THING_UUID']
+    }
+  ]
+}
+
+# command_valueはecnonetliteのweb APIの仕様書と対応している（はず(海原さん))
+
+payload_set = {
+  "requests": [
+    {
+      "command": [
+        {
+        "command_type": "character", # IoT-EX社の定義しているものなので、自分で作れない？それかEchonetliteの定義式？
+        "command_code": "set_property_value",
+        #######
+        # SET内容を指定してください（operationStatus, operationMode, ...)
+        #######
+        "command_value": "instantaneousChargingAndDischargingElectricPower"
+        }
+      ],
+      #######
+      # 遠方操作対象のDER機器を指定してください（DRIVER_ID, R_EDGE_ID, THINGS_UUID）
+      #######
+      "driver_id": os.environ['DRIVER_ID'],
+      "r_edge_id": os.environ['R_EDGE_ID'],
+      "thing_uuid": os.environ['THING_UUID']
+    }
+  ]
+}
+
+headers = {
+  "Content-type": "application/json",
+  "Authorization": "Bearer "+os.environ['ACCESS_TOKEN'],
+  "X-IOT-API-KEY": os.environ['API_KEY']
+}
+
+try:
+    response_get1 = requests.request("POST", url, headers=headers, json=payload_get, timeout=20)
+    # print(response_get1.text)
+    jsonData = response_get1.json()
+
+    # ecnonetliteの応答が使用上の都合で複数あるので、それに対応するためのコード。
+    # 今回は、[0]のみを想定しているので、帰ってきたリストの先頭のものだけを取得している。
+    get1 = {
+    'command_code': jsonData['results'][0]["command"][0]["command_code"],
+    'command_value': jsonData['results'][0]["command"][0]["command_value"],
+    'response_result': jsonData['results'][0]["command"][0]["response"][0]["response_result"],
+    'response_value': jsonData['results'][0]["command"][0]["response"][0]["response_value"]
+    }
+
+    print(get1["command_code"]  + " (" + get1["command_value"] + ") ..." + 
+    get1["response_result"] + " (" + get1["response_value"] + ")")
+
+except TimeoutError:
+    print("get1 is timed out")
+    pass
+
+try:
+    response_set = requests.request("POST", url, headers=headers, json=payload_set, timeout=20)
+    print(response_set.text)
+    jsonData = response_set.json()
+
+    set1 = {
+    'command_code': jsonData['results'][0]["command"][0]["command_code"],
+    'command_value': jsonData['results'][0]["command"][0]["command_value"],
+    'response_result': jsonData['results'][0]["command"][0]["response"][0]["response_result"],
+    'response_value': jsonData['results'][0]["command"][0]["response"][0]["response_value"]
+    }
+
+    print(set1["command_code"]  + " (" + set1["command_value"] + ") ..." + 
+    set1["response_result"] + " (" + set1["response_value"] + ")")
+
+except TimeoutError:
+    print("set is timed out")
+    pass
+
+time.sleep(5)
+
+try:
+    response_get2 = requests.request("POST", url, headers=headers, json=payload_get, timeout=20)
+    # print(response_get2.text)
+    jsonData = response_get2.json()
+
+    get2 = {
+    'command_code': jsonData['results'][0]["command"][0]["command_code"],
+    'command_value': jsonData['results'][0]["command"][0]["command_value"],
+    'response_result': jsonData['results'][0]["command"][0]["response"][0]["response_result"],
+    'response_value': jsonData['results'][0]["command"][0]["response"][0]["response_value"]
+    }
+
+    print(get2["command_code"]  + " (" + get2["command_value"] + ") ..." + 
+    get2["response_result"] + " (" + get2["response_value"] + ")")
+except TimeoutError:
+    print("get2 is timed out")
+    pass
